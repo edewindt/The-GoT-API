c5c5657dac902407fc5cc3b9ff31d645624ed240
edewindt <edewindt27@outlook.com>
2022-08-30 12:01:16 -0500

    added all core object types

--- schema/schema.js
+++ schema/schema.js
@@ -9,6 +9,7 @@ const CharacterType = new GraphQLObjectType({
     name:"Character",
     description:"Describes a Character",
     fields: () => ({
+    id:{type: new GraphQLNonNull(GraphQLString)},
     firstname: {type: new GraphQLNonNull(GraphQLString)},
     lastname: {type: new GraphQLNonNull(GraphQLString)},
     fullname: {type: new GraphQLNonNull(GraphQLString)},
@@ -17,4 +18,19 @@ const CharacterType = new GraphQLObjectType({
 //   familyid:String,
 //   mediaid:String,
     })
+})
+
+const FamilyType = new GraphQLObjectType({
+    name: "Family",
+    description: "Describes a House or Family",
+    fields: () =>({
+    house: {type: new GraphQLNonNull(GraphQLString)},
+    region: {type: new GraphQLNonNull(GraphQLString)},
+    sigil: {type: new GraphQLNonNull(GraphQLString)},
+    blazon: {type: new GraphQLNonNull(GraphQLString)},
+    seat: {type: new GraphQLNonNull(GraphQLString)},
+    words:{type: GraphQLString},
+    origin: {type: GraphQLString},
+    notes: {type: GraphQLString}
+})
 })
