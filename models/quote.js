28bbd5b595e66fc19a27d0dc8c67d030a2feff03
edewindt <edewindt27@outlook.com>
2022-08-30 08:26:23 -0500

    First schema added, models folder added

---
+++ models/quote.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const quoteSchema = new Schema({
+  body: String,
+  episode: Number,
+  season: Number,
+  author: String 
+});
+
+module.exports = mongoose.model("Quote", quoteSchema);
